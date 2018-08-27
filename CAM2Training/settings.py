c74ad2f6ee36ea57a3387a9f3f4004e470f76c11
waynewhbaaa <wang2940@purdue.edu>
2018-08-27 02:28:25 -0400

    another5

--- CAM2Training/settings.py
+++ CAM2Training/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f_)5rrc-lkqb!z9rw+%0*)cqh%+_py$)(sly^kj7jchx!*ff3('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -113,11 +113,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+import os
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
